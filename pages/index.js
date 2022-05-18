e1c01cfcd0d6ab69a938a891206cb067858a5ede
ABIPRAVI <darkparadise877@gmail.com>
2022-05-18 09:21:23 +0530

    flex ui apply for all pages and components

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect, createContext } from "react";
 import Head from "next/head";
 import Home1 from "../components/db/db";
 import Login from "./company/login";
-import Script from "next/script";
 
 export default function Home() {
 	const [AUTHKEY, setAUTHKEY] = useState("");
@@ -28,28 +27,9 @@ export default function Home() {
 					content="Shop App created by Abipravi"
 				/>
 				<link rel="icon" href="/download.png" />
-				<link
-					href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-					rel="stylesheet"
-					integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-					crossOrigin="anonymous"
-				></link>
+				
 			</Head>
-			<Script
-				src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-				integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-				crossorigin="anonymous"
-			></Script>
-			<Script
-				src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
-				integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
-				crossorigin="anonymous"
-			></Script>
-			<Script
-				src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
-				integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
-				crossorigin="anonymous"
-			></Script>
+			
 			{AUTHKEY !== "" ? <Home1 /> : <Login authcheck={findAuth} />}
 		</>
 	);
